be4efa6092d065df90b6b11fbd8dd042503fc199
Seyba <seybakhalifadembele@gmail.com>
2022-12-18 16:08:21 +0100

    installaton de la librairie pandas

--- scrap_book.py
+++ scrap_book.py
@@ -1,10 +1,17 @@
 import requests
 from bs4 import BeautifulSoup
-import csv
+import pandas as pd
 
 BASE_URL = "http://books.toscrape.com/"
 HEADERS = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/108.0.0.0 Safari/537.36 Edg/108.0.1462.46"}
-LIVRE_1 = "catalogue/the-black-maria_991/index.html"
+
+response = requests.get(BASE_URL)
+parser = BeautifulSoup(response.content, "html.parser")
+h3 = parser.find_all("h3")
+a = h3[9].find("a")
+product_page_url = a["href"]
+
+
 def extraire_donnees(url):
     response = requests.get(url, headers=HEADERS)
     if response.status_code == 200:
@@ -16,6 +23,7 @@ def extraire_donnees(url):
 
 
 
+
     table = soup.find("table" ,class_= "table")
     upc =  table.find("td").text
 
@@ -23,9 +31,10 @@ def extraire_donnees(url):
     title = soup.find("h1").text
 
 
-    table = soup.find("table", class_="table")
+    table = soup.find("table", class_="table table-striped")
     tds = table.find_all("td")
     price_including_tax= tds[3].text
+    
 
     price_excluding_tax = tds[2].text
 
@@ -51,21 +60,12 @@ def extraire_donnees(url):
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
     infos_livre = [ product_page_url, upc, title, price_including_tax, price_excluding_tax,
                     number_available,  product_description, category, review_rating, image_url]
-extraire_donnees(BASE_URL + LIVRE_1)
+
+
+extraire_donnees(BASE_URL + product_page_url)
+
+
+
     
