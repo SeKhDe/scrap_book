4269b1f557b5c2b5b7e6fe26aac255f5c4aa0744
Seyba <seybakhalifadembele@gmail.com>
2022-12-20 17:35:27 +0100

    creation des fonctions extract_book et extract_category OK

--- scrap_book.py
+++ scrap_book.py
@@ -3,95 +3,112 @@ from bs4 import BeautifulSoup
 import pandas as pd
 
 
-HEADERS = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/108.0.0.0 Safari/537.36 Edg/108.0.1462.46"}
 
+BASE_URL = "http://books.toscrape.com/"
 BASE_URL_category = "http://books.toscrape.com/catalogue/"
-
-urls_category_fiction = []
-url = "http://books.toscrape.com/catalogue/category/books/fiction_10/page-1.html"
+url_fiction = "http://books.toscrape.com/catalogue/category/books/fiction_10/"
 
 product_page_url_s = []
-upcs = []
-titles = []
-price_including_tax_s = []
-price_excluding_tax_s = []
-number_available_s = []
-product_description_s = []
-category_s= []
-review_rating_s = []
-image_url_s= []
 
-#boucle des pages de la categorie fiction
-for i in range(1,5):
-    url_category_fiction = f"http://books.toscrape.com/catalogue/category/books/fiction_10/page-{i}.html"
-    urls_category_fiction.append(url_category_fiction)
+#Fonction extraction d'une categorie
 
+def extract_category(url):
+    page = True
+    i = 1
+    liste_url_category = []
+    while page:
+        url_category = f"{url}page-{i}.html"
+        response = requests.get(url_category)
+        if response.status_code == 200:
+            html = response.content
+            soup = BeautifulSoup(html, "html.parser")
+            i += 1
+        else:
+            page = False
+
+
+
+        ol_class = soup.find("ol", class_="row")
+        li_class = ol_class.find_all("li", class_="col-xs-6")
+        for li in li_class:
+            a = li.find("a")
+            a = BASE_URL_category + a["href"].replace("../../../", "")
+            if not a in liste_url_category:
+                liste_url_category.append(a)
+
+
+
+
+    return liste_url_category
+
+
+
+
+
+#Fonction extraction d'un livre
+def extract_book(url):
     response = requests.get(url)
     if response.status_code == 200:
         html = response.content
         soup = BeautifulSoup(html, "html.parser")
 
+    upcs = []
+    table = soup.find("table", class_="table")
+    upc = table.find("td").text
+    upcs.append(upc)
 
-    liste_url = []
-    li_class = soup.find("ol", class_="row").find_all("li","col-xs-6")
-    for li in li_class:
-        a = li.find("a")
-        a = BASE_URL_category + a["href"].replace("../../../", "")
+    titles = []
+    title = soup.find("h1").text
+    titles.append(title)
 
-        liste_url.append(a)
+    price_including_tax_s = []
+    table = soup.find("table", class_="table table-striped")
+    tds = table.find_all("td")
+    price_including_tax = tds[3].text
+    price_including_tax_s.append(price_including_tax)
 
-    for book_url in liste_url:
-        response = requests.get(book_url)
-        if response.status_code == 200:
-            html = response.content
-            soup = BeautifulSoup(html, "html.parser")
+    price_excluding_tax_s = []
+    price_excluding_tax = tds[2].text
+    price_excluding_tax_s.append(price_excluding_tax)
 
+    number_available_s = []
+    number_available = tds[5].text
+    number_available_s.append(number_available)
 
-        product_page_url_s.append(book_url)
+    product_description_s = []
+    product_description_article = soup.find("article", class_="product_page")
+    product_description = product_description_article.find("p", recursive=None)
+    product_description_s.append(product_description)
 
+    category_s = []
+    ul_breadcrumb = soup.find("ul", class_="breadcrumb")
+    categorys = ul_breadcrumb.find_all("li")
+    category = categorys[2].text
+    category_s.append(category)
 
-        table = soup.find("table", class_="table")
-        upc = table.find("td").text
-        upcs.append(upc)
+    review_rating_s = []
+    div_product = soup.find("div", class_="product_main")
+    p_start_rating = div_product.find("p", class_="star-rating")
+    review_rating = p_start_rating.attrs["class"][1]
+    review_rating_s.append(review_rating)
 
-        title = soup.find("h1").text
-        titles.append(title)
+    image_url_s = []
+    div_item = soup.find("div", class_="item")
+    balise_img = div_item.find("img")
+    image_url_src = balise_img["src"]
+    image_url = image_url_src.replace("../../", BASE_URL_category)
+    image_url_s.append(image_url)
 
-        table = soup.find("table", class_="table table-striped")
-        tds = table.find_all("td")
-        price_including_tax = tds[3].text
-        price_including_tax_s.append(price_including_tax)
-
-        price_excluding_tax = tds[2].text
-        price_excluding_tax_s.append(price_excluding_tax)
-
-        number_available = tds[5].text
-        number_available_s.append(number_available)
-
-        product_description_article = soup.find("article", class_="product_page")
-        product_description = product_description_article.find("p", recursive=None).text
-        product_description_s.append(product_description)
-
-        ul_breadcrumb = soup.find("ul", class_="breadcrumb")
-        categorys = ul_breadcrumb.find_all("li")
-        category = categorys[2].text
-        category_s.append(category)
-
-        div_product = soup.find("div", class_="product_main")
-        p_start_rating = div_product.find("p", class_="star-rating")
-        review_rating = p_start_rating.attrs["class"][1]
-        review_rating_s.append(review_rating)
-
-        div_item = soup.find("div", class_="item")
-        balise_img = div_item.find("img")
-        image_url_src = balise_img["src"]
-        image_url = image_url_src.replace("../../", BASE_URL_category)
-        image_url_s.append(image_url)
-
-        infos_livre = {"product_page_url": product_page_url_s,"upc": upcs, "title": titles, "price_including_tax": price_including_tax_s, "price_excluding_tax": price_excluding_tax_s,
-                       "number_available": number_available_s, "product_description": product_description_s, "category": category_s, "review_rating":  review_rating_s,
-                       "image_url": image_url_s}
+    infos_livre = {"product_page_url": product_page_url_s, "upc": upcs, "title": titles,
+                   "price_including_tax": price_including_tax_s, "price_excluding_tax": price_excluding_tax_s,
+                   "number_available": number_available_s, "product_description": product_description_s,
+                   "category": category_s, "review_rating": review_rating_s,
+                   "image_url": image_url_s}
 
 
+    return infos_livre
 
 
+#print(extract_book("http://books.toscrape.com/catalogue/when-im-gone_95/index.html"))
+print(len(extract_category(url_fiction)))
+print(extract_category(url_fiction))
